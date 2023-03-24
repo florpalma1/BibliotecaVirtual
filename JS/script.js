f16a313a0abe9e5f087154ad502c098eb3e79e7d
florpalma1 <contr.lucascordoba98@gmail.com>
2023-03-24 15:05:00 -0300

    agregue seccion ingresados recientemente, todo responsive

--- JS/script.js
+++ JS/script.js
@@ -82,3 +82,26 @@ var swiper = new Swiper(".feautured-slider", {
          },
        },
      });
+
+    //  swipper arrivals
+
+     var swiper = new Swiper(".arrivals-slider", {
+      spaceBetween:10,
+      loop:true,
+      centeredSlides: true,
+      autoplay: {
+         delay: 9500,
+         disableOnInteraction: false,
+      },
+         breakpoints: {
+           0: {
+             slidesPerView: 1,
+           },
+           768: {
+              slidesPerView: 2,
+            },
+           1024: {
+             slidesPerView: 3,
+           },
+         },
+       });
