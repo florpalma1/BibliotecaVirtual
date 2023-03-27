4b8f4016073c0dbe1ea341fb6a782f611350bdd5
florpalma1 <contr.lucascordoba98@gmail.com>
2023-03-27 16:05:49 -0300

    agregue seccion blogs responsive

--- JS/script.js
+++ JS/script.js
@@ -129,3 +129,26 @@ var swiper = new Swiper(".feautured-slider", {
              },
            },
          });
+
+        //  swipper blogs
+        var swiper = new Swiper(".blogs-slider", {
+          spaceBetween:10,
+          grabCursor:true,
+          loop:true,
+          centeredSlides: true,
+          autoplay: {
+             delay: 9500,
+             disableOnInteraction: false,
+          },
+             breakpoints: {
+               0: {
+                 slidesPerView: 1,
+               },
+               768: {
+                  slidesPerView: 2,
+                },
+               1024: {
+                 slidesPerView: 3,
+               },
+             },
+           });
