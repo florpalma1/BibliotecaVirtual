d228312f7a7a7a546ad852f7bcffafc9e8424c02
florpalma1 <contr.lucascordoba98@gmail.com>
2023-03-27 15:26:54 -0300

    agegue seccion de reviews responsive

--- JS/script.js
+++ JS/script.js
@@ -105,3 +105,27 @@ var swiper = new Swiper(".feautured-slider", {
            },
          },
        });
+
+      //  swipper reviews
+
+      var swiper = new Swiper(".review-slider", {
+        spaceBetween:10,
+        grabCursor:true,
+        loop:true,
+        centeredSlides: true,
+        autoplay: {
+           delay: 9500,
+           disableOnInteraction: false,
+        },
+           breakpoints: {
+             0: {
+               slidesPerView: 1,
+             },
+             768: {
+                slidesPerView: 2,
+              },
+             1024: {
+               slidesPerView: 3,
+             },
+           },
+         });
